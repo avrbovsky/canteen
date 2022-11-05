04c7bb119feb4c34f230fbca63734a3ec57dcf9d
Adrian Vrbovsky <avrbovsky64@gmail.com>
2022-11-05 12:36:31 +0100

    Added node version, dockerignore file and Dockerfile

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,4 @@
 import { Header } from "./components/Header";
-import {Row} from 'reactstrap';
 
 type Props = {
   children: JSX.Element;
@@ -8,8 +7,10 @@ type Props = {
 export const Layout = ({ children }: Props): JSX.Element => {
   return (
     <>
-    <Header />
-    <div style={{height: '90vh', backgroundColor: 'lightgray', paddingTop: '40px'}}>{children}</div>
+      <Header />
+      <div style={{height: '90vh', backgroundColor: 'lightgray', paddingTop: '40px'}}>
+        {children}
+      </div>
     </>
   );
 };
