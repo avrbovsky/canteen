63fcae66c40edddf1c40e06c886ff1acce6e3dfe
Adrián Vrbovský <avrbovsky64@gmail.com>
2022-11-13 12:26:57 +0100

    Refactored LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,82 +1,31 @@
-import React, { useState, useEffect, ReactElement } from "react";
-import { Input, Button, Form } from "reactstrap";
+import { useState } from "react";
+import { Input, Button, Form, FormGroup, FormFeedback } from "reactstrap";
 import { url } from "../config";
 
 export const LoginPage = () => {
-
-  const [input, setInput] = useState({
-    username: '',
-    password: '',
-  });
-
-  const [error, setError] = useState({
-    username: '',
-    password: '',
-  })
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleLogin = () => {
     fetch(`${url}/login`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-        "username": input.username,
-        "password": input.password
+        username: username,
+        password: password,
       }),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
     })
-    .then(response => response.json())
-    .then(result => {
-    })
-    .catch(error => console.log('error', error));
-}
-
-  const onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void = (
-    event
-  ) => {
-    const { name, value } = event.target;
-    setInput(prev => ({
-      ...prev,
-      [name]: value
-    }));
-    validateInput(event);
-  }
-
-  const validateInput: (event: React.ChangeEvent<HTMLInputElement>) => void = (
-    event
-  ) => {
-    let { name, value } = event.target;
-    setError(prev => {
-      const stateObj = { ...prev, [name]: "" };
-
-      switch (name) {
-        case "username":
-          if (!value) {
-            stateObj[name] = "Please enter Username.";
-          }
-          break;
-
-        case "password":
-          if (!value) {
-            stateObj[name] = "Please enter Password.";
-          }
-          break;
-
-        default:
-          break;
-      }
-
-      return stateObj;
-    });
-  }
-  
+      .then((response) => response.json())
+      .then((result) => {})
+      .catch((error) => console.log("error", error));
+  };
 
   return (
-    
     <div
       style={{
         width: "80%",
-        backgroundColor: "gray",
         marginLeft: "auto",
         marginRight: "auto",
         textAlign: "center",
@@ -86,28 +35,34 @@ export const LoginPage = () => {
       <h3>Login</h3>
 
       <Form>
-        <Input
-          type="text"
-          name="username"
-          placeholder='Enter Username'
-          value={input.username}
-          onChange={onInputChange}
-          onBlur={validateInput}
-          required></Input>
-        {error.username && <span className='err'>{error.username}</span>}
-
-        <Input
-          type="password"
-          name="password"
-          placeholder='Enter Password'
-          value={input.password}
-          onChange={onInputChange}
-          onBlur={validateInput}
-          required></Input>
-        {error.password && <span className='err'>{error.password}</span>}
-        <br/>
-        <Button nClick={handleLogin}>Submit</Button>
+        <FormGroup>
+          <Input
+            type="text"
+            name="username"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+            required
+            invalid={!username}
+          ></Input>
+          <FormFeedback>Enter username</FormFeedback>
+        </FormGroup>
+        <FormGroup>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            required
+            invalid={!password}
+          ></Input>
+          <FormFeedback>Enter Password</FormFeedback>
+        </FormGroup>
+        <Button onClick={handleLogin} disabled={!password || !username}>
+          Submit
+        </Button>
       </Form>
     </div>
   );
-}
+};
